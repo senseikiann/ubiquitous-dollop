7f66a0a414864b03f718aa6fb005c74ea77948da
senseikiann <141477806+senseikiann@users.noreply.github.com>
2026-02-23 20:22:57 +0330

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,6 @@
 #define GPIOC_BSRR  (*(volatile unsigned int*)0x40011010)
 #define GPIOC_BRR   (*(volatile unsigned int*)0x40011014)
 
-void delay(volatile unsigned int d)
-{
-    while(d--);
-}
-
 int main(void)
 {
     RCC_APB2ENR |= (1 << 4);
@@ -15,12 +10,9 @@ int main(void)
     GPIOC_CRH &= ~(0xF << 20);
     GPIOC_CRH |= (0x2 << 20);
 
+    GPIOC_BRR = (1 << 13);
+
     while(1)
     {
-        GPIOC_BRR = (1 << 13);
-        delay(800000);
-
-        GPIOC_BSRR = (1 << 13);
-        delay(800000);
     }
 }

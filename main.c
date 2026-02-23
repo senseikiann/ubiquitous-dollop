b43a23ce396964ee2e7977add448eb3cb2ab08eb
senseikiann <141477806+senseikiann@users.noreply.github.com>
2026-02-23 19:49:19 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
+#include "stm32f1xx.h"
+
+void delay(volatile uint32_t d)
+{
+    while(d--);
+}
+
 int main(void)
 {
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    GPIOC->CRH &= ~(0xF << ((13 - 8) * 4));
+    GPIOC->CRH |= (0x2 << ((13 - 8) * 4));
+
     while(1)
     {
+        GPIOC->BSRR = (1 << 13);
+        delay(800000);
+
+        GPIOC->BRR = (1 << 13);
+        delay(800000);
     }
 }
